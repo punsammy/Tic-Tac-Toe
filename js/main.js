5f68ca339b03900de609898835d897eb0260afe3
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-30 23:52:57 -0400

    alternate colors and add window alert when cell has already been given class

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(function(){
 
+  var turn = 1;
 
   $('.cell').on('click', function(){
-    $(this).addClass('x');
+    if ($(this).hasClass('x') || $(this).hasClass('o')) {
+      window.alert('Nope');
+    } else {
+    if (turn % 2 === 0) {
+      $(this).addClass('x')
+    } else {
+      $(this).addClass('o');
+    }
+    turn++;
+    }
   });
 
 });
